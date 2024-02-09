c31036e7f30d5bee243aef0ad6168f424cddaceb
Celeste Monter <celesteemonterxd@gmail.com>
2024-02-08 19:41:55 -0700

    second commit

---
+++ src/main/java/com/example/dog/Dog.java
@@ -0,0 +1,25 @@
+package com.example.dog;
+
+public class Dog {
+
+    //Atributos
+
+    public String name;
+    public String raza;
+    public String altura;
+
+    //Operaciones
+
+    void comer()
+    {
+
+    }
+    void dormir()
+    {
+
+    }
+    void ladrar()
+    {
+
+    }
+}
