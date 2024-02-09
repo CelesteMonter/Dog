d8d1f20a2784afc41554a50114bec8039771abe7
Carlos Alvarado <car.alv.27@gmail.com>
2024-02-08 21:01:12 -0700

    v3

--- src/main/java/com/example/dog/Dog.java
+++ src/main/java/com/example/dog/Dog.java
@@ -3,17 +3,46 @@ package com.example.dog;
 public class Dog {
     public String name;
     public String breed;
-    public String height;
+    public Double height;
+    public Double weight;
+    // 0 female, 1 male
+    public Integer gender;
+    public Integer age;
 
-    void eat() {
-
+    Dog(String name, String breed, Double height, Double weight, Integer gender, Integer age) {
+        this.name = name;
+        this.breed = breed;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+        this.age = age;
     }
 
-    void sleep() {
-
+    String getName() {
+        return name;
     }
 
-    void bark() {
+    String getBreed() {
+        return breed;
+    }
 
+    String getHeight() {
+        return height.toString();
+    }
+
+    String getWeight() {
+        return weight.toString();
+    }
+
+    String getGender() {
+        if (gender == 0) {
+            return "Female";
+        } else {
+            return "Male";
+        }
+    }
+
+    String getAge() {
+        return age.toString();
     }
 }
--- src/main/java/com/example/dog/HelloController.java
+++ src/main/java/com/example/dog/HelloController.java
@@ -1,8 +1,40 @@
 package com.example.dog;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    @FXML
+    TextField nameField = new TextField();
+    @FXML
+    TextField breedField = new TextField();
+    @FXML
+    TextField heightField = new TextField();
+    @FXML
+    TextField weightField = new TextField();
+    @FXML
+    TextField genderField = new TextField();
+    @FXML
+    TextField ageField = new TextField();
+
+    @FXML
+    TextArea textArea = new TextArea();
+
+
+    @FXML
+    protected void setDog() {
+        Dog dog = new Dog(
+                nameField.getText(),
+                breedField.getText(),
+                Double.parseDouble(heightField.getText()),
+                Double.parseDouble(weightField.getText()),
+                Integer.parseInt(genderField.getText()),
+                Integer.parseInt(ageField.getText())
+        );
+
+        textArea.setText(dog.name);
+    }
+
 
 }
